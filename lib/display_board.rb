eca291f00c40a173d7e8f87e06e6c967559593d9
Joseph Kao <josephtkao@gmail.com>
2020-03-28 00:10:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,4 @@ def display_board(array)
   put(" #{array[0]} | #{array[1]} | #{array[2] }"
   put(" #{array[3]} | #{array[4]} | #{array[5]} ")
   put(" #{array[6]} | #{array[7]} | #{array[8]} ")
+end
