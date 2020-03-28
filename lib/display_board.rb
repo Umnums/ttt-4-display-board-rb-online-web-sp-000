e06795c9e80fbf34a7aed6bb0f6dc4e000085af3
Joseph Kao <josephtkao@gmail.com>
2020-03-28 00:12:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(array)
-  put(" #{array[0]} | #{array[1]} | #{array[2] }")
-  put(" #{array[3]} | #{array[4]} | #{array[5]} ")
-  put(" #{array[6]} | #{array[7]} | #{array[8]} ")
+  puts(" #{array[0]} | #{array[1]} | #{array[2] }")
+  puts(" #{array[3]} | #{array[4]} | #{array[5]} ")
+  puts(" #{array[6]} | #{array[7]} | #{array[8]} ")
 end
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
